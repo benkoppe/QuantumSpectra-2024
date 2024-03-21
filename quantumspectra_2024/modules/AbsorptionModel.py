15f1beef607e588c59bfd51938cd3a93461010fd
benkoppe <bentastic1@gmail.com>
2024-03-21 01:50:10 -0700

    AbsorptionModel point features

---
+++ quantumspectra_2024/modules/AbsorptionModel.py
@@ -0,0 +1,13 @@
+import jax
+import jax_dataclasses as jdc
+
+from abc import ABC
+
+
+@jdc.pytree_dataclass
+class AbsorptionModel(ABC):
+    """Represents a model for generating absorption spectra"""
+
+    start_energy: jdc.Static[float] = 0.0
+    end_energy: jdc.Static[float] = 20_000.0
+    num_points: jdc.Static[int] = 2_001
