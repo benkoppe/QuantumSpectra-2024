c7b1194e2ae913c92da190fa67841dfc3a1957a5
benkoppe <bentastic1@gmail.com>
2024-03-21 00:33:24 -0700

    save file stub

--- quantumspectra_2024/modules/AbsorptionSpectrum.py
+++ quantumspectra_2024/modules/AbsorptionSpectrum.py
@@ -2,5 +2,15 @@ import jax
 
 
 class AbsorptionSpectrum:
+    """Represents an absorption spectrum. Outputted by all `AbsorptionModel` subclasses."""
+
     energies: jax.Array
     intensities: jax.Array
+
+    def save_file(self, filename: str) -> None:
+        """Save the absorption spectrum to a file.
+
+        Args:
+            filename (str): output filename
+        """
+        pass
