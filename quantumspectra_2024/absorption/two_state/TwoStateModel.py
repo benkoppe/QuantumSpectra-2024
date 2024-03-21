4c757ae74b71693712907ac8662c24c2e8d7344b
benkoppe <bentastic1@gmail.com>
2024-03-21 02:21:04 -0700

    two state class with parameters

---
+++ quantumspectra_2024/absorption/two_state/TwoStateModel.py
@@ -0,0 +1,20 @@
+import jax_dataclasses as jdc
+from jaxtyping import Float, Int, Array, Scalar
+
+from quantumspectra_2024.modules.AbsorptionModel import AbsorptionModel as Model
+
+
+@jdc.pytree_dataclass
+class TwoStateModel(Model):
+    """A two-state quantum mechanical model for absorption spectra."""
+
+    temperature_kelvin: Float[Scalar, ""]
+    broadening: Float[Scalar, ""]
+
+    basis_sets: jdc.Static[Int[Array, "2"]]
+
+    transfer_integral: Float[Scalar, ""]
+    energy_gap: Float[Scalar, ""]
+
+    mode_frequencies: Float[Array, "num_modes"]
+    mode_couplings: Float[Array, "num_modes"]
