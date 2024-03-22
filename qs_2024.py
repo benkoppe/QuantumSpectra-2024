a1fb358f9d685acb020159255d8c165c05b9264f
Ben <Koppe.Development@gmail.com>
2024-03-22 05:50:11 -0700

    reading spectrum from config file

---
+++ qs_2024.py
@@ -0,0 +1,24 @@
+from typing import Type
+
+from quantumspectra_2024.modules.Config import (
+    parse_config,
+    initialize_absorption_from_config,
+)
+from quantumspectra_2024.modules.absorption import AbsorptionModel, AbsorptionSpectrum
+
+from quantumspectra_2024.absorption.two_state import TwoStateModel
+
+
+def main():
+    config: dict = parse_config("Compute absorption spectrum with a given config file.")
+
+    str_to_model: dict = {
+        "two_state": TwoStateModel,
+    }
+    model: AbsorptionModel = initialize_absorption_from_config(config, str_to_model)
+
+    spectrum: AbsorptionSpectrum = model.get_absorption()
+
+
+if __name__ == "__main__":
+    main()
