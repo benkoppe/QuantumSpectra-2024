c481ea48dde7125a521067443cfae1b17da74444
Ben <Koppe.Development@gmail.com>
2024-03-21 17:00:22 -0700

    added abstract get_absorption method

--- quantumspectra_2024/modules/absorption/AbsorptionModel.py
+++ quantumspectra_2024/modules/absorption/AbsorptionModel.py
@@ -1,7 +1,9 @@
 import jax_dataclasses as jdc
 from jaxtyping import Float, Int, Scalar
 
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from quantumspectra_2024.modules.absorption.AbsorptionSpectrum import AbsorptionSpectrum
 
 
 @jdc.pytree_dataclass
@@ -11,3 +13,8 @@ class AbsorptionModel(ABC):
     start_energy: jdc.Static[Float[Scalar, ""]] = 0.0
     end_energy: jdc.Static[Float[Scalar, ""]] = 20_000.0
     num_points: jdc.Static[Int[Scalar, ""]] = 2_001
+
+    @abstractmethod
+    def get_absorption(self) -> AbsorptionSpectrum:
+        """Compute the absorption spectrum for the model."""
+        pass
